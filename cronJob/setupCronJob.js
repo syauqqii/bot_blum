fa08367385df3f140443600a4f527843a7b35c91
S Y A U Q I <57295427+syauqqii@users.noreply.github.com>
2024-09-06 17:19:36 +0700

    Fix: Typo

--- cronJob/setupCronJob.js
+++ cronJob/setupCronJob.js
@@ -4,7 +4,7 @@ const { claimFarmReward } = require('../api');
 function setupCronJob(token) {
     return new Promise((resolve, reject) => {
         try {
-            const job = new cron.CronJob('0 */10 * * *', async () => {
+            const job = new cron.CronJob('0 */12 * * *', async () => {
                 console.log(' [-] Starting farming session every 10 hours...');
 
                 try {
@@ -16,7 +16,7 @@ function setupCronJob(token) {
             });
 
             job.start();
-            console.log(' [-] Cron job set up to run every 10 hours.');
+            console.log(' [-] Cron job set up to run every 12 hours.');
             resolve();
         } catch (error) {
             reject(error);
