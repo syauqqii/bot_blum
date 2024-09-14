0f40722ae8834623c47bdda6cbd304b5a59c7970
S Y A U Q I <57295427+syauqqii@users.noreply.github.com>
2024-09-14 19:43:48 +0700

    Chore: Update getTasks.js API endpoint

--- api/getTasks.js
+++ api/getTasks.js
@@ -2,12 +2,12 @@ const axios = require('axios');
 
 async function getTasks(token) {
     const { data } = await axios({
-        url: 'https://game-domain.blum.codes/api/v1/tasks',
+        url: 'https://earn-domain.blum.codes/api/v1/tasks',
         method: 'GET',
         headers: { Authorization: token },
     });
 
-    return data[0].subSections;
+    return data[0].tasks;
 }
 
 module.exports = getTasks;
