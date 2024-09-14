66004b73a5a246cee72660c06bd51235583bc45f
S Y A U Q I <57295427+syauqqii@users.noreply.github.com>
2024-09-14 19:45:01 +0700

    Chore: Update claimTaskReward.js API endpoint

--- api/claimTaskReward.js
+++ api/claimTaskReward.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 async function claimTaskReward(token, taskId) {
     const { data } = await axios({
-        url: `https://game-domain.blum.codes/api/v1/tasks/${taskId}/claim`,
+        url: `https://earn-domain.blum.codes/api/v1/tasks/${taskId}/claim`,
         method: 'POST',
         headers: { Authorization: token },
         data: null,
